5576295bde9adddfc903fae85d9391ef8a4b2158
Fishykiller <24575547+Fishykiller@users.noreply.github.com>
2017-08-09 11:22:29 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import py2exe, sys, os
 sys.argv.append('py2exe')
 
 setup(
-    options = {'py2exe': {'bundle_files': 1, 'compressed': True}},
+    options = {'py2exe': {'bundle_files': 1, 'compressed': True, 'includes': ["email.mime.multipart","email.mime.text"] }},
     windows = [{'script': "RWW2.pyw"}],
     zipfile = None,
 )
